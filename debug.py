7b626e7b64cb801c504a6c9a125cb8674a426f2e
dmitriy <di-man.94@mail.ru>
2025-04-19 15:32:55 +0400

    added test forms

---
+++ debug.py
@@ -0,0 +1,41 @@
+NCR = [{
+            value: "Delhi",
+            label: "Delhi"
+        }, {
+            value: "Gurgaon",
+            label: "Gurgaon"
+        }, {
+            value: "Noida",
+            label: "Noida"
+        }]
+Pradesh = [{
+            value: "Agra",
+            label: "Agra"
+        }, {
+            value: "Lucknow",
+            label: "Lucknow"
+        }, {
+            value: "Merrut",
+            label: "Merrut"
+        }],
+        Haryana: [{
+            value: "Karnal",
+            label: "Karnal"
+        }, {
+            value: "Panipat",
+            label: "Panipat"
+        }],
+        Rajasthan: [{
+            value: "Jaipur",
+            label: "Jaipur"
+        }, {
+            value: "Jaiselmer",
+            label: "Jaiselmer"
+        }]
+    };
+
+
+a = ["Hindi", "English", "Maths", "Physics", "Chemistry", "Biology",
+"Computer Science", "Commerce", "Accounting", "Economics", "Arts", "Social Studies",
+"History", "Civics"]
+
